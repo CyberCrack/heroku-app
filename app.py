c5115d548eca6026ad490ef6cbc2275dfd47c0e4
Cyber Crack <mayankanuragi2013@gmail.com>
2020-08-04 00:29:19 +0530

    make it better

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ def home():
 	return """
 	<a href="https://heroku-app-us-admissions.herokuapp.com/">Home</a>
 	<h1>Hello<h1>
-	<h4> Please use the following format to get the data<h4>
-	<h6> https://heroku-app-us-admissions.herokuapp.com/api/<GRE>/<GPA>/<RANK><h6>
-	<h6> In place of <GRE> enter something like 500 and so on in each of the 3 values within range<h6>
-	<h6> 0<=GRE<=800 <h6>
-	<h6> 0<=GPA<=4 <h6>
-	<h6> 1<=RANK<=4 <h6>
-	<h6> Eg. type "https://heroku-app-us-admissions.herokuapp.com/api/500/3/2" this in url <h6>
-	<h6> If return value 0 then not admitted else if 1 then admitted<h6>
+	<h1> Please use the following format to get the data<h1>
+	<h1> https://heroku-app-us-admissions.herokuapp.com/api/<GRE>/<GPA>/<RANK><h1>
+	<h1> In place of <GRE> enter something like 500 and so on in each of the 3 values within range<h1>
+	<h1> 0<=GRE<=800 <h1>
+	<h1> 0<=GPA<=4 <h1>
+	<h1> 1<=RANK<=4 <h1>
+	<h1> Eg. type "https://heroku-app-us-admissions.herokuapp.com/api/500/3/2" this in url <h1>
+	<h1> If return value 0 then not admitted else if 1 then admitted<h1>
 	
 	"""
 @app.route('/api/<int:gre>/<gpa>/<int:rank>',methods=['GET'])
