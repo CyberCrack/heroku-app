a7d278fedbcbd18e0b66d69bcc4f7fdebbfc9628
Cyber Crack <mayankanuragi2013@gmail.com>
2020-08-03 23:54:58 +0530

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from DeployableModel import getPredictions
 app = Flask(__name__)
-
+@app.route('/')
+def home():
+	return "<H1>Hello</H1>"
 @app.route('/api/<int:gre>/<gpa>/<int:rank>',methods=['GET'])
 def test(gre,gpa,rank):
 	print(gre,gpa,rank)
@@ -9,4 +11,4 @@ def test(gre,gpa,rank):
 
 # driver function
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
