4f12d56517e2e7717f1256d08e4005fb5d7a7472
Cyber Crack <mayankanuragi2013@gmail.com>
2020-08-04 14:51:07 +0530

    make it better

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
+
 from DeployableModel import getPredictions
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def home():
 	return """
@@ -18,10 +22,21 @@ def home():
 	<h1> If return value 0 then not admitted else if 1 then admitted<h1>
 	
 	"""
-@app.route('/api/<int:gre>/<gpa>/<int:rank>',methods=['GET'])
-def test(gre,gpa,rank):
-	print(gre,gpa,rank)
-	return jsonify(getPredictions(gre=gre,gpa=gpa,rank=rank))
+
+
+@app.route('/api/<int:gre>/<gpa>/<int:rank>', methods=['GET'])
+def test(gre, gpa, rank):
+	if not gpa.isnumeric():
+		return jsonify("ERROR")
+	if not 0 <= gre <= 800:
+		return jsonify("ERROR")
+	gpa = float(gpa)
+	if not 0 <= gpa <= 4:
+		return jsonify("ERROR")
+	if not 1 <= rank <= 4:
+		return jsonify("ERROR")
+	return jsonify(getPredictions(gre=gre, gpa=gpa, rank=rank))
+
 
 # driver function
 if __name__ == '__main__':
