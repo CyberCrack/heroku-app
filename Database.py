bbbd9db99a7dc4b446de81bd9c42fb6edba7c98d
Cyber Crack <mayankanuragi2013@gmail.com>
2020-08-18 20:08:41 +0530

    Adding POST,PUT,DELETE requests and Cloud DB Sqlite3

---
+++ Database.py
@@ -0,0 +1,66 @@
+import os
+import sqlite3
+
+databaseExits = True
+if not os.path.exists("usadmissions.db"):
+	databaseExits = False
+conn = sqlite3.connect('usadmissions.db', check_same_thread=False)
+cursor = conn.cursor()
+
+
+def createDatabase():
+	sql = '''CREATE TABLE admissions(
+	   id INTEGER PRIMARY KEY AUTOINCREMENT,
+	   gre INTEGER NOT NULL,
+	   gpa REAL NOT NULL,
+	   student_rank INTEGER NOT NULL,
+	   admit INTEGER
+	)'''
+	cursor.execute(sql)
+
+
+def insertData(gre, gpa, student_rank, admit):
+	cursor.execute("INSERT INTO admissions (gre,gpa,student_rank,admit) VALUES ( ?, ?, ?, ?	)", (gre, gpa, student_rank, admit))
+	conn.commit()
+
+
+def deleteData(id):
+	cursor.execute("DELETE FROM admissions WHERE id=?", (id,))
+	conn.commit()
+
+
+def getRecentID():
+	cursor.execute("SELECT seq FROM SQLITE_SEQUENCE WHERE name='admissions';")
+	return cursor.fetchall()[0][0]
+
+
+if not databaseExits:
+	createDatabase()
+
+
+def updateGPA(gpa, id):
+	cursor.execute("UPDATE admissions SET gpa = ? WHERE id = ?", (gpa, id))
+
+	conn.commit()
+	return cursor.rowcount
+
+
+def updategre(gre, id):
+	cursor.execute("UPDATE admissions SET gre = ? WHERE id = ?", (gre, id))
+
+	conn.commit()
+	return cursor.rowcount
+
+
+def updaterank(rank, id):
+	cursor.execute("UPDATE admissions SET student_rank = ? WHERE id = ?", (rank, id))
+
+	conn.commit()
+	return cursor.rowcount
+
+
+def deleteRecord(id):
+	cursor.execute("UPDATE admissions SET student_rank = ? WHERE id = ?", (id,))
+
+	conn.commit()
+	return cursor.rowcount
--- app.py
+++ app.py
@@ -1,27 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, url_for, render_template
 
+import Database
 from DeployableModel import getPredictions
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return """
-	<a href="https://heroku-app-us-admissions.herokuapp.com/">Home</a>
-	<h1>Hello<h1>
-	<h1> Please use the following format to get the data<h1>
-	<h1> https://heroku-app-us-admissions.herokuapp.com/api/&lt;GRE&gt;/&lt;GPA&gt;/&lt;RANK&gt; <h1>
-	<h1> In place of &lt;GRE&gt; enter something like 100 and so on in each of the 3 values within range<h1>
-	<h1> Allowed Range<h1>
-	<h1> 0<=GRE<=800 <h1>
-	<h1> 0<=GPA<=4 <h1>
-	<h1> 1<=RANK<=4 <h1>
-	<h1> Eg. type <a href="https://heroku-app-us-admissions.herokuapp.com/api/100/2/4">"https://heroku-app-us-admissions.herokuapp.com/api/100/2/4"</a> in url box or click to see this example.<h1>
-	Eg. type <a href="https://heroku-app-us-admissions.herokuapp.com/api/650/4/2">"https://heroku-app-us-admissions.herokuapp.com/api/650/4/2"</a> in url box or click to see this example.<h1>
-	<h1> If return value 0 then not admitted else if 1 then admitted<h1>
-	
-	"""
+	if request.method == "POST":
+		recv_gre = request.form['gre']
+		recv_gpa = request.form['gpa']
+		recv_rank = request.form['rank']
+		try:
+			return redirect(url_for('test', gre=recv_gre, gpa=recv_gpa, rank=recv_rank))
+		except Exception:
+			return jsonify("ERROR")
+	return render_template("home.html")
 
 
 @app.route('/api/<int:gre>/<gpa>/<int:rank>', methods=['GET'])
@@ -35,7 +30,120 @@ def test(gre, gpa, rank):
 		return jsonify("ERROR")
 	if not 1 <= rank <= 4:
 		return jsonify("ERROR")
-	return jsonify(getPredictions(gre=gre, gpa=gpa, rank=rank))
+	result = getPredictions(gre=gre, gpa=gpa, rank=rank)
+	Database.insertData(gre=gre, gpa=gpa, student_rank=rank, admit=result)
+	return jsonify(result)
+
+
+@app.route('/api', methods=['POST'])
+def postAPI():
+	recv_data = request.get_json()
+	try:
+		gre = int(recv_data['gre'])
+		gpa = float(recv_data['gpa'])
+		rank = int(recv_data['rank'])
+	except Exception:
+		return jsonify("ERROR")
+	if not 0 <= gre <= 800:
+		return jsonify("ERROR")
+	if not 0 <= gpa <= 4:
+		return jsonify("ERROR")
+	if not 1 <= rank <= 4:
+		return jsonify("ERROR")
+	result = getPredictions(gre=gre, gpa=gpa, rank=rank)
+	Database.insertData(gre=gre, gpa=gpa, student_rank=rank, admit=result)
+	studentID = Database.getRecentID()
+	return jsonify(StudentID=studentID, Prediction=result)
+
+
+@app.route('/api', methods=['PUT'])
+def putAPI():
+	hasKeys = {'gre': True, 'gpa': True, 'rank': True}
+	recv_data: dict = request.get_json()
+	updateCount = 0
+	try:
+		id = int(recv_data['id'])
+	except Exception as e:
+		return jsonify("ERROR")
+
+	try:
+		gre = int(recv_data['gre'])
+	except KeyError:
+		hasKeys['gre'] = False
+	except ValueError:
+		return jsonify("ERROR")
+	except Exception as e:
+		return jsonify("ERROR")
+
+	try:
+		gpa = float(recv_data['gpa'])
+	except KeyError:
+		hasKeys['gpa'] = False
+	except ValueError:
+		return jsonify("ERROR")
+	except Exception as e:
+		return jsonify("ERROR")
+
+	try:
+		rank = int(recv_data['rank'])
+	except KeyError:
+		hasKeys['rank'] = False
+	except ValueError:
+		return jsonify("ERROR")
+	except Exception as e:
+		return jsonify("ERROR")
+
+	if hasKeys['gre']:
+		if not 0 <= gre <= 800:
+			return jsonify("ERROR")
+
+	if hasKeys['gpa']:
+		if not 0 <= gpa <= 4:
+			return jsonify("ERROR")
+
+	if hasKeys['rank']:
+		if not 1 <= rank <= 4:
+			return jsonify("ERROR")
+
+	for key in list(recv_data.keys()):
+		if key == 'gre':
+			try:
+				updateCount += Database.updategre(gre=gre, id=id)
+			except Exception:
+				return jsonify("ERROR")
+		if key == 'gpa':
+			try:
+				updateCount += Database.updateGPA(gpa=gpa, id=id)
+			except Exception:
+				return jsonify("ERROR")
+		if key == 'rank':
+			try:
+				updateCount += Database.updaterank(rank=rank, id=id)
+			except Exception:
+				return jsonify("ERROR")
+	if True not in hasKeys.values():
+		return jsonify("ERROR")
+	studentID = id
+	if list(hasKeys.values()).count(True) == updateCount:
+		return jsonify(StudentID=studentID, Sucessfull=True)
+	else:
+		return jsonify("ERROR")
+
+
+@app.route('/api', methods=['DELETE'])
+def deleteAPI():
+	recv_data: dict = request.get_json()
+	try:
+		id = int(recv_data['id'])
+	except Exception:
+		return jsonify("ERROR")
+
+	studentID = id
+	updateCount = Database.deleteData(id=studentID)
+	if updateCount != 0:
+		return jsonify(StudentID=studentID, Sucessfull=True)
+	else:
+		return jsonify("ERROR")
 
 
 # driver function
